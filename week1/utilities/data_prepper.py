5a24f38b181811df3bce4684b313459805acb976
Suneel Marthi <smarthi@apache.org>
2022-06-26 22:52:36 +0000

    Week 1 checkin - Level 1 complete

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -232,20 +232,26 @@ class DataPrepper:
                                                 size=len(query_doc_ids), terms_field=terms_field)
         ##### Step Extract LTR Logged Features:
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
+        response = self.opensearch.search(body=log_query, index=self.index_name)
+        hits = response['hits']['hits']
         feature_results = {}
         feature_results["doc_id"] = []  # capture the doc id so we can join later
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
         feature_results["name_match"] = []
+
+        print(hits[0])
         rng = np.random.default_rng(12345)
-        for doc_id in query_doc_ids:
-            feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
-            feature_results["query_id"].append(query_id)
-            feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
+
+        if len(hits) > 0:
+            for hit in hits:
+                feature_results["doc_id"].append(hit["_id"])
+                feature_results["query_id"].append(query_id)
+                feature_results["sku"].append(hit["_source"]["sku"][0])
+                feature_results["name_match"].append(hit["fields"]["_ltrlog"][0]["log_entry"][0].get("value"))
+
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
         # IMPLEMENT_END
